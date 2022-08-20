b66f7cc2a1e25ebf52e63fef3a252c5bd1b75d12
sunqiiii <108568986+sunqiiii@users.noreply.github.com>
2022-08-20 21:32:39 +0800

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,19 +1,16 @@
-class question {
-  constructor(question, answer) {
-    this.question = question;
-    this.answer = answer;
-    this.correct = false;
-  }
+document.getElementById('p1').ondrag = function() {
+let ele = document.getElementById('p1');
+ele.innerHTML = 'I have been dragged! Alert the police!'; 
 }
-
-let quiz = [];
-
-quiz.push(new question('What is 3+5?', 'more than six?'));
-quiz.push(new question('What color is the sky?', 'blue'));
-quiz.push(new question('Is ice cold?', 'yes'));
-
-let i = 0;
-while (i < quiz.length)  {
-  console.log(quiz[i].question + ' ' + quiz[i].answer);
-  ++i
+document.getElementById('p2').onmousedown = function() {
+let ele = document.getElementById('p2');
+ele.innerHTML = 'I have been clicked! Alert the police!'; 
+}
+document.getElementById('p3').onmouseup = function() {
+let ele = document.getElementById('p3');
+ele.innerHTML = 'I have been clicked! Alert the police!'; 
+}
+document.getElementById('p4').onmouseover = function() {
+let ele = document.getElementById('p4');
+ele.innerHTML = 'Stop hovering! Alert the police!'; 
 }
