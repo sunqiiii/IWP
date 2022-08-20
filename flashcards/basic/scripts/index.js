98e47534e1da7ab006c4b42ac25370cb69b620b7
sunqiiii <108568986+sunqiiii@users.noreply.github.com>
2022-08-20 10:20:24 +0800

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,23 +1,17 @@
-//Declaration for the class child
-//
 class question {
-// A constructor is a special function used to create an instance of the class
-constructor(question, answer) {
-this.question = question;
-this.answer = answer;
-// We will set this to true when answered correctly.
+  constructor (question, answer) {
+    this.question = question;
+    this.answer = answer;
+  }
 }
-}
-// Declare a new array
+
 let quiz = [];
-// Create three questions and push them onto the quiz array
 quiz.push(new question('What is 3+5?', 'more than six?'));
 quiz.push(new question('What color is the sky?', 'blue'));
 quiz.push(new question('Is ice cold?', 'yes'));
 
-// Loop through the array printing out the question and the answer
 let i = 0;
 while (i < quiz.length)  {
-console.log(quiz[i].question + ' ' + quiz[i].answer);
-++i
+  console.log(quiz[i].question + ' ' + quiz[i].answer);
+  ++i
 }
